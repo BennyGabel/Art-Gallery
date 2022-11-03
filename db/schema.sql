9035be1877b95f650a1035cd395e0615a5df1b36
13 Miles <bennygabel@gmail.com>
2022-11-02 21:27:03 -0400

    updating db directory and populating info:db/  connection.js, db.sql, schema.sql, seeds.sql

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS art;
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS voters;
+
+CREATE TABLE art ( 
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  is_Public   TINYINT (1),
+  obj_Id      INT (10),
+  gallery_Num INT (10),
+  department  CHAR(50),
+  obj_Name    CHAR(50),
+  title       CHAR(120),
+  Culture     CHAR(75),
+  artist_Name TEXT(280),
+  artist_Nation CHAR(115),
+  endBy       CHAR(190),
+  medium      CHAR(240),
+  city        CHAR(15),
+  linkResource CHAR(60));
+
+
+
+-- id	Is Public Domain	Object ID	Gallery Number	Department	Object Name	Title	Culture	Artist Display Name	Artist Nationality	Artist End Date	Medium	City	Link Resource
