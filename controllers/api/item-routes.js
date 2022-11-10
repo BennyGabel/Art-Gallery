576ed49feb70b7f75d1e657abcb9c62f4a5b7520
13 Miles <bennygabel@gmail.com>
2022-11-10 15:10:48 -0500

    Add sorting for All items

--- controllers/api/item-routes.js
+++ controllers/api/item-routes.js
@@ -3,6 +3,70 @@ const sequelize = require('../../config/connection');
 const { Art, Item, User, Comment } = require('../../models');
 
 
+// Add sorting for-when showing  All - BEG
+router.get('/sort/:sort', (req, res) => {
+
+  // Evaluate SORT Selection - BEG 
+  // Will receive Sort Parameter
+  evalSortParam = req.params.sort
+
+  fSortBy = `endby`
+//    switch (LOWER(evalSortParam))  {    "LOWER" DOESN'T WORK
+  switch (evalSortParam)  {
+    case 'title':
+      fSortBy = `title`
+      break 
+    case 'department':
+      fSortBy = `department`
+      break 
+    fSortBy = `culture`
+      break 
+    case 'artistnation':
+      fSortBy = `artistnation`
+      break 
+    case 'endby':
+      fSortBy = `endby`
+      break 
+
+  }  
+  // Evaluate SORT Selection - END
+
+  Item.findAll({ 
+  attributes: [
+    'id',
+    'cobjid',
+    'title',
+    'department',
+    'culture',
+    'artistnation',
+    'endby',
+    'linkresource'
+  ] ,
+    // order: [['endby', 'DESC']],
+    order: [[`${fSortBy}`, 'DESC']],   // Assign Sort-by by parameter
+    include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'item_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]      
+})
+  .then(dbItemData => res.json(dbItemData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+// Add sorting - END
 
 // get all users
 router.get('/', (req, res) => {
