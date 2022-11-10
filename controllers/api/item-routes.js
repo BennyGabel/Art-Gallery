a1908d7363e19c0666a486d5e62ad2946f82c4b6
13 Miles <bennygabel@gmail.com>
2022-11-10 09:35:50 -0500

    Add two routes - item-routes

--- controllers/api/item-routes.js
+++ controllers/api/item-routes.js
@@ -91,6 +91,107 @@ router.get('/:id', (req, res) => {
     });
 });
 
+// Add Searches
+// Two Searches - BEG
+router.get('/culture/:culture/endby/:endby', (req, res) => {
+  console.log('Culture parameter')
+  Item.findAll({
+     where: {
+       culture: req.params.culture,
+       endby: req.params.endby
+     },
+     attributes: [
+       'id',
+       'cobjid',
+       'title',
+       'department',
+       'culture',
+       'artistnation',
+       'endby',
+       'linkresource'
+       //[sequelize.literal('(SELECT COUNT(*) FROM vote WHERE item.id = vote.post_id)'), 'vote_count']
+     ],
+     include: [
+       {
+         model: Comment,
+         attributes: ['id', 'comment_text', 'item_id', 'user_id', 'created_at'],
+         include: {
+           model: User,
+           attributes: ['username']
+         }
+       },
+       {
+         model: User,
+         attributes: ['username']
+       }
+     ]
+   })
+     .then(dbItemData => {
+       if (!dbItemData) {
+         res.status(404).json({ message: 'No item found with this id' });
+         return;
+       }
+       res.json(dbItemData);
+     })
+     .catch(err => {
+       console.log(err);
+      res.status(500).json(err);
+     });
+ });
+
+// Two Searches - END
+
+
+
+
+// Culture search is now working
+router.get('/culture/:culture', (req, res) => {
+   console.log('Culture parameter')
+   Item.findAll({
+      where: {
+        culture: req.params.culture
+      },
+      attributes: [
+        'id',
+        'cobjid',
+        'title',
+        'department',
+        'culture',
+        'artistnation',
+        'endby',
+        'linkresource'
+        //[sequelize.literal('(SELECT COUNT(*) FROM vote WHERE item.id = vote.post_id)'), 'vote_count']
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'item_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbItemData => {
+        if (!dbItemData) {
+          res.status(404).json({ message: 'No item found with this id' });
+          return;
+        }
+        res.json(dbItemData);
+      })
+      .catch(err => {
+        console.log(err);
+       res.status(500).json(err);
+      });
+  });
+
+// by culture - END - 22.11.09 
+// Add Searches
 
 router.put('/:id', (req, res) => {
   Item.update(
